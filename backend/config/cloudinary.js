bd4314d94b537b484d44ab6d0fd499c838d1fd8d
Anukulsingh01 <anukulsinghbisht@gmail.com>
2026-02-27 23:26:11 +0530

    fixed some issues

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -26,17 +26,37 @@
     
 // }
 // export default uploadOnCloudinary
-import { v2 as cloudinary } from 'cloudinary'
-import fs from 'fs'
-console.log("NAME:", process.env.CLOUDINARY_NAME)
-console.log("KEY:", process.env.CLOUDINARY_API_KEY)
-console.log("SECRET:", process.env.CLOUDINARY_API_SECRET)
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY , 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-})
+// import { v2 as cloudinary } from 'cloudinary'
+// import fs from 'fs'
+// console.log("NAME:", process.env.CLOUDINARY_NAME)
+// console.log("KEY:", process.env.CLOUDINARY_API_KEY)
+// console.log("SECRET:", process.env.CLOUDINARY_API_SECRET)
+// cloudinary.config({ 
+//   cloud_name: process.env.CLOUDINARY_NAME, 
+//   api_key: process.env.CLOUDINARY_API_KEY , 
+//   api_secret: process.env.CLOUDINARY_API_SECRET 
+// })
 
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+// Manually point to the .env file if it's one folder up
+dotenv.config(); 
+
+// console.log("NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("KEY:", process.env.CLOUDINARY_API_KEY)
+// console.log("SECRET:", process.env.CLOUDINARY_API_SECRET)
+
+// Should print now!
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+// ... rest of your code
 const uploadOnCloudinary = async (filePath) => {
   try {
     if (!filePath) return null
